76cd4b69ebf5b0ed8f5bffd6457b1f414b85a569
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-06 14:41:00 +0530

    [Priyanshu] Add: Added Constructor for Private methods in ContactBooks.cs

--- ContactsBook.cs
+++ ContactsBook.cs
@@ -22,5 +22,56 @@ namespace AddressBookMain
         private string userZip;
         private string phoneNumber;
         private string email;
+    
+        public String UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string UserAddress
+        {
+            get { return userAddress; }
+            set { userAddress = value; }
+        }
+        public string UserCity
+        {
+            get { return userCity; }
+            set { userCity = value; }
+        }
+
+        public string UserState
+        {
+            get { return userState; }
+            set { userState = value; }
+        }
+
+        public string UserZip
+        {
+            get { return userZip; }
+            set { userZip = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
     }
 }
