25bc99890d6fe440b190ad7014e1842723988f36
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-07 15:23:15 +0530

    [Priyanshu] Fix: Fixed Functionality DisplayOption

--- Program.cs
+++ Program.cs
@@ -17,26 +17,18 @@ namespace AddressBookMain
             {
                 case "1":
                     UserInputs.takeUserInputs();
-                    Console.Clear();
                     break;
                 case "2":
                     AddressBookOperations.DisplayUserData(ContactsBook.UserData);
-                    if (ContactsBook.UserData == null)
-                    {
-                        Console.WriteLine("Please Add User Before Displaying");
-                    }
-                    Console.Clear();
                     break;
                 case "3":
                     AddressBookOperations.EditUser();
-                    Console.Clear();
                     break;
                 case "4":
                     AddressBookOperations.DeleteUser();
                     Console.Clear();
                     break;
                 case "exit":
-                    Console.Clear();
                     Console.WriteLine("Exiting Program");
                     Console.WriteLine("Thank you for using service :) ");
                     Thread.Sleep(2000);
@@ -48,15 +40,16 @@ namespace AddressBookMain
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("What do you want to do ");
-            Console.WriteLine("Enter '1' for Add User");
-            Console.WriteLine("Enter '2' for Displaying User");
-            Console.WriteLine("Enter '3' For Editing Existing User");
-            Console.WriteLine("Enter '4' For Delete user");
-            Console.WriteLine("Enter Exit to Exit AddressBook");
             while (exit != true)
             {
+                Console.WriteLine("What do you want to do ");
+                Console.WriteLine("Enter '1' for Add User");
+                Console.WriteLine("Enter '2' for Displaying User");
+                Console.WriteLine("Enter '3' For Editing Existing User");
+                Console.WriteLine("Enter '4' For Delete user");
+                Console.WriteLine("Enter Exit to Exit AddressBook");
                 DisplayOptions(Console.ReadLine());
+                Console.Clear();
             }
         }
     }
