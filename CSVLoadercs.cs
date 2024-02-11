5ede36183c6909c0d00860e7164913a8f3852999
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-11 19:23:25 +0530

    [Priyanshu] Add: Added a class file to load CSV data to UserData

---
+++ CSVLoadercs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookMain
+{
+    internal class CSVLoadercs
+    {
+        public static void loadCSVToDictionary(string CSVFilePath)
+        {
+            using (var reader = new StreamReader(CSVFilePath))
+            {
+
+            }
+        }
+    }
+}
