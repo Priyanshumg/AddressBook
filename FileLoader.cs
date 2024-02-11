c1923327f383c586bf4772e8832d109a5323f7ee
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-12 00:29:15 +0530

    [Priyanshu] Add: Added class item FileLoader

---
+++ FileLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookMain
+{
+    internal class FileLoader
+    {
+        public static void LoadDataToTxt()
+        {
+            var SampleTextFileLocation = @"C:\Users\Dell\Desktop\locobuzz .net training\AddressBookMain\Sample.txt";
+            using (StreamWriter file = new StreamWriter(SampleTextFileLocation))
+            {
+                Console.Write("{ ");
+                foreach (var UserNames in ContactsBook.UserData)
+                {
+                    string username = UserNames.Key;
+                    Dictionary<string, string> userData = UserNames.Value;
+                    // Console.WriteLine($"For the User: {UserNames.Key}");
+                    foreach (var dataEntry in userData)
+                    {
+                        file.Write($"{username}: ");
+                        file.WriteLine($"{dataEntry.Key}: {dataEntry.Value}");
+                    }
+                    file.WriteLine();
+                }
+                Console.WriteLine(" }");
+            }
+        }
+    }
+}
