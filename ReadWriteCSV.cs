9ad1b7b62a6ccf757e16f0b2e3ea2b203f595ba5
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-12 11:51:14 +0530

    [Priyanshu] Add: Added Functionality to Write a CSV File

--- ReadWriteCSV.cs
+++ ReadWriteCSV.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
 
 namespace AddressBookMain
 {
     internal class ReadWriteCSV
     {
-
+        public static void WriteCSV()
+        {
+            var SampleTextFileLocation = @"C:\Users\Dell\Desktop\locobuzz .net training\AddressBookMain\Book1.csv";
+            using (StreamWriter file = new StreamWriter(SampleTextFileLocation))
+            {
+                foreach (var UserNames in ContactsBook.UserData)
+                {
+                    // FirstLine
+                    string username = UserNames.Key;
+                    file.WriteLine($"UserName, {username}");
+                    Dictionary<string, string> userData = UserNames.Value;
+                    foreach (var dataEntry in userData)
+                    {
+                        file.Write($" ,{dataEntry.Key}, {dataEntry.Value}");
+                        file.WriteLine();
+                    }
+                    file.WriteLine();
+                }
+                file.Close();
+            }
+        }
     }
 }
